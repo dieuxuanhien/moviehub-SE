cea2e1cd0663b0e6f19cb91adb832e6ee304a5fa
Xuan-Hien Dieu <dieuxuanhien@gmail.com>
2025-09-22 20:02:37 +0700

    Initial commit

---
+++ apps/movie-hub/jest.config.ts
@@ -0,0 +1,10 @@
+export default {
+  displayName: 'movie-hub',
+  preset: '../../jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/movie-hub',
+};
