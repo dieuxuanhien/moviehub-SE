1cf8dbe5ad3de19367624a040df96d3d91ed514b
Lê Hùng Chính <chinhkkmmzz@gmail.com>
2025-09-30 20:06:04 +0700

    feat(cinema-service): implement Prisma service and update cinema module and service for database integration

--- apps/cinema-service/src/app/cinema/cinema.module.ts
+++ apps/cinema-service/src/app/cinema/cinema.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { CinemaController } from './cinema.controller';
 import { CinemaService } from './cinema.service';
+import { PrismaService } from '../prisma.service';
 
 @Module({
   controllers: [CinemaController],
-  providers: [CinemaService]
+  providers: [CinemaService, PrismaService],
 })
 export class CinemaModule {}
