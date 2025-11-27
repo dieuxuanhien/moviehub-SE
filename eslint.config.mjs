a7c6c4954cb3336c776812fa70dd3ce95da9a3cb
Xuan-Hien Dieu <dieuxuanhien@gmail.com>
2025-11-27 15:44:24 +0700

    seed test

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -14,7 +14,7 @@ export default [
         'error',
         {
           enforceBuildableLibDependency: true,
-          allow: ['^.*/eslint(\\.base)?\\.config\\.[cm]?[jt]s$', '^@movie-hub/.*'],
+          allow: ['^.*/eslint(\\.base)?\\.config\\.[cm]?[jt]s$'],
           depConstraints: [
             {
               sourceTag: '*',
