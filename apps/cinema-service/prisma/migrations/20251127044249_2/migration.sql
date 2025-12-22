1c4e5d40b9f2f6e0fc2076752a222215fdaa4468
Lê Hùng Chính <chinhkkmmzz@gmail.com>
2025-12-22 17:20:03 +0700

    chore(prisma): reset and rename latest Prisma migrations for all services

--- apps/cinema-service/prisma/migrations/20251127044249_2/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE "public"."CinemaReviews" ALTER COLUMN "user_id" SET DATA TYPE VARCHAR;
-
--- AlterTable
-ALTER TABLE "public"."SeatReservations" ALTER COLUMN "user_id" SET DATA TYPE VARCHAR;
